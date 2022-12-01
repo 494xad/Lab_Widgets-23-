f1369d5fc0f1b1c08e86a9174c363daf5a3b633c
494xad <494xad@mail.ru>
2022-12-02 01:31:52 +0700

    Second widget

---
+++ win.cpp
@@ -0,0 +1,35 @@
+﻿#include "win.h"
+
+Win::Win(QWidget *parent) : QWidget(parent)
+{
+    setWindowTitle("Счётчик"); // заголовок "Счетчик"
+
+    label1 = new QLabel("Cчет по 1", this); // надпись "Счет по 1"
+    label2 = new QLabel("Cчет по 5", this); // надпись "Счет по 5"
+    edit1 = new Counter("0", this); // результаты
+    edit2 = new Counter("0", this);
+    calcbutton = new QPushButton("+1", this); //кнопки
+    exitbutton = new QPushButton("Выход", this);
+
+    QHBoxLayout *layout1 = new QHBoxLayout(); // горизонтальный менеджер компоновки для надписей
+	layout1->addWidget(label1);
+	layout1->addWidget(label2);
+
+    QHBoxLayout *layout2 = new QHBoxLayout(); // горизонтальный менеджер компоновки для полей чисел
+	layout2->addWidget(edit1);
+	layout2->addWidget(edit2);
+
+    QHBoxLayout *layout3 = new QHBoxLayout(); // горизонтальный менеджер компоновки для кнопок
+	layout3->addWidget(calcbutton);
+	layout3->addWidget(exitbutton);
+
+    QVBoxLayout *layout4 = new QVBoxLayout(this); // вертикальный менеджер компоновки для трёх горизонтальных компоновщиков (извиняюсь за тавтологию)
+	layout4->addLayout(layout1);
+	layout4->addLayout(layout2);
+	layout4->addLayout(layout3);
+
+	// связь сигнала нажатия кнопки и слота закрытия окна
+	connect(calcbutton, &QPushButton::clicked, edit1, &Counter::add_one);
+	connect(edit1, &Counter::tick_signal, edit2, &Counter::add_one);
+	connect(exitbutton, &QPushButton::clicked, this, &Win::close);
+}
---
+++ win.h
@@ -0,0 +1,52 @@
+﻿#ifndef WIN_H
+#define WIN_H
+
+#include <QBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QWidget>
+
+class Counter : public QLineEdit
+{
+	Q_OBJECT
+
+public:
+	Counter(const QString &contents, QWidget *parent = nullptr) : QLineEdit(contents, parent) {}
+
+signals:
+    void tick_signal();// сигнал, вызываемый при счёте кратном пяти
+
+public slots:
+	void add_one()
+	{
+        QString str = text();
+        int r = str.toInt(); // перевод введенного числа из строки в int
+
+        if (r != 0 && r % 5 == 0) // вызов сигнала, если число кратно 5
+        {
+            emit tick_signal();
+        }
+
+        r++; // +1 в графе "счёт по 5"
+
+		str.setNum(r);
+        setText(str); // вывод результата
+	}
+};
+
+class Win : public QWidget
+{
+	Q_OBJECT
+
+protected:
+    QLabel *label1, *label2; // надписи
+    Counter *edit1, *edit2; // числа
+    QPushButton *calcbutton; // кнопки
+	QPushButton *exitbutton;
+
+public:
+    Win(QWidget *parent = nullptr); // окно
+};
+
+#endif // WIN_H
